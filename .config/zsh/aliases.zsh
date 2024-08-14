5ac6415ca7f2409d86ecd435000691fb781cdd00
Timothy May <tmay@policyexpert.co.uk>
2024-08-14 21:37:04 +0100

    Remove: work specific tap

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,4 +1,6 @@
-alias brewup="brew update && brew bundle install --cleanup --no-lock && brew upgrade"
+# Disabling cleanup for now till I can find a nice way to merge Brewfiles
+# alias brewup="brew update && brew bundle install --cleanup && brew upgrade"
+alias brewup="brew update && brew bundle install && brew upgrade"
 
 alias docker-remove-containers='docker container rm $(docker container ls -qa)'
 alias docker-remove-images='docker image rm $(docker image ls -qa)'
--- .config/zsh/environment.zsh
+++ .config/zsh/environment.zsh
@@ -3,6 +3,11 @@ export ZSH_CONFIG="$HOME/.config/zsh"
 export PATH="$PATH:/Users/timothy.may/.local/bin"
 
 export EDITOR=hx
+
+# Docker / colima
 export DOCKER_HOST=unix:///Users/timothy.may/.colima/default/docker.sock
 export TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE=/var/run/docker.sock
+
+# Homebrew bundle
 export HOMEBREW_BUNDLE_FILE=~/.config/Brewfile
+export HOMEBREW_BUNDLE_NO_LOCK=1
