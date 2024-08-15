e019707e391443b0df90aa74da62170927649744
Timothy May <tmay@policyexpert.co.uk>
2024-08-15 09:33:27 +0100

    Update: replace references to explicit home directory with /Users/timothy.may

--- .config/zsh/environment.zsh
+++ .config/zsh/environment.zsh
@@ -2,14 +2,14 @@ export ZSH_CONFIG="$HOME/.config/zsh"
 
 # Add binaries to path
 export PATH="$PATH:/usr/local/bin"
-export PATH="$PATH:/Users/timothy.may/.local/bin"
+export PATH="$PATH:$HOME/.local/bin"
 
 export EDITOR=hx
 
 # Docker / colima
-export DOCKER_HOST=unix:///Users/timothy.may/.colima/default/docker.sock
+export DOCKER_HOST="unix://$HOME/.colima/default/docker.sock"
 export TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE=/var/run/docker.sock
 
 # Homebrew bundle
-export HOMEBREW_BUNDLE_FILE=~/.config/Brewfile
+export HOMEBREW_BUNDLE_FILE="$HOME/.config/Brewfile"
 export HOMEBREW_BUNDLE_NO_LOCK=1
