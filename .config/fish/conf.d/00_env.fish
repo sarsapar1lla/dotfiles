2dd164caf47db0851a7bd2d51ef76829e7fdbc5e
Tim May <98321934+qmg-tmay@users.noreply.github.com>
2024-12-30 14:19:17 +0000

    Remove: unnecessary path appends (#22)
    Update: move editor setting to separate helix config file

--- .config/fish/conf.d/00_env.fish
+++
@@ -1,7 +0,0 @@
-# Add binaries to path
-set -gx PATH $PATH /usr/local/bin
-set -gx PATH $PATH "$HOME/.local/bin"
-set -gx PATH /usr/local/sbin $PATH
-set -gx PATH $PATH "$HOME/.cargo/bin"
-
-set -gx EDITOR hx
---
+++ .config/fish/conf.d/helix.fish
@@ -0,0 +1,5 @@
+if command --query hx
+    set -gx EDITOR hx
+else
+    echo "'hx' is not installed on path"
+end
