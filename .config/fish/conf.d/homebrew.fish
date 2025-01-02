2be611493bb6b3d0f8eb4e02ec8ec85a2048c929
Timothy May <tmay@policyexpert.co.uk>
2025-01-02 10:31:50 +0000

    Update: include required path appends

--- .config/fish/conf.d/homebrew.fish
+++ .config/fish/conf.d/homebrew.fish
@@ -1,6 +1,9 @@
 if command --query brew
     set -gx HOMEBREW_BUNDLE_FILE "$HOME/.config/brew/main/Brewfile"
     set -gx HOMEBREW_BUNDLE_NO_LOCK 1
+
+    set -gx PATH $PATH /usr/local/bin
+    set -gx PATH $PATH /usr/local/sbin
 else
     echo "'brew' is not installed on path"
 end
