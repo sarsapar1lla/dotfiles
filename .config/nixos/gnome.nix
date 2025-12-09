c32a81b2730d23c70a239cf09ae24dc03a615c17
Tim <timothy_may@hotmail.co.uk>
2025-12-09 22:28:17 +0000

    Add: gnome config file

---
+++ .config/nixos/gnome.nix
@@ -0,0 +1,38 @@
+{ config, ... }:
+{
+  programs.dconf.profiles.user.databases = [
+    {
+      lockAll = true;
+      settings = {
+        "org/gnome/desktop/background" = {
+          picture-options = "zoom";
+          picture-uri-dark = "file://${config.users.users.timo.home}/.config/nixos/assets/itb.png";
+        };
+
+        "org/gnome/desktop/interface" = {
+          accent-color = "orange";
+          color-scheme = "prefer-dark";
+          clock-format = "24h";
+          clock-show-weekday = true;
+        };
+
+        "org/gnome/settings-daemon/plugins/color" = {
+          night-light-enabled = true;
+          night-light-schedule-automatic = true;
+        };
+        
+        "org/gnome/shell" = {
+          favorite-apps = [
+            "org.gnome.Nautilus.desktop"
+            "Alacritty.desktop"
+            "google-chrome.desktop"
+          ];
+        };
+
+        "org/gtk/gtk4/Settings/FileChooser" = {
+          show-hidden = true;
+        };
+      };
+    }    
+  ];
+}
