1ffeb5bc2b8ddb965cddf708c720d66b240487bf
Timothy May <tmay@policyexpert.co.uk>
2025-12-10 10:05:59 +0000

    Update: fix bat config

--- .config/fish/functions/cat.fish
+++ .config/fish/functions/cat.fish
@@ -1,6 +1,6 @@
 function cat -d "use bat instead of cat"
     if command --query bat
-        bat --theme Catppuccin-mocha $argv
+        bat $argv
     else
         echo "'bat' is not installed on path. Falling back to 'cat'"
         command cat $argv
