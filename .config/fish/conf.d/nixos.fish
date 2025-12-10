90a70eea187b7a87597a1815878c9d910bc086ed
Tim <timothy_may@hotmail.co.uk>
2025-12-10 22:37:25 +0000

    Update: rebuild switch

--- .config/fish/conf.d/nixos.fish
+++ .config/fish/conf.d/nixos.fish
@@ -1,4 +1,4 @@
 if command --query nixos-rebuild
     set -l NIXOS_FLAKE_PATH $(readlink -f "$HOME/.config/nixos/flake.nix" | xargs -- dirname)
-    abbr nrb "nixos-rebuild test --flake '$NIXOS_FLAKE_PATH#zenith' --sudo"
+    abbr nrb "nixos-rebuild switch --flake '$NIXOS_FLAKE_PATH#zenith' --sudo"
 end
