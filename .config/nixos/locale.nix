4a6e0c2d0650f28862252b607fa312c5358ab8fc
Tim <timothy_may@hotmail.co.uk>
2025-12-10 18:21:26 +0000

    Update: use us keyboard layout

--- .config/nixos/locale.nix
+++ .config/nixos/locale.nix
@@ -20,10 +20,10 @@
 
   # Configure keymap in X11
   services.xserver.xkb = {
-    layout = "gb";
+    layout = "us";
     variant = "";
   };
 
   # Configure console keymap
-  console.keyMap = "uk";
+  console.keyMap = "us";
 }
