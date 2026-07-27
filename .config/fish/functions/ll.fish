52818e9ffc3708a9a1d6fddce8db9fb1c956d0d4
Timothy May <tmay@policyexpert.co.uk>
2026-07-27 10:25:11 +0100

    Update: ll function to specify icons always

--- .config/fish/functions/ll.fish
+++ .config/fish/functions/ll.fish
@@ -1,6 +1,6 @@
 function ll -d "alias eza -la"
     if command --query eza
-        eza -la --icons $argv
+        eza -la --icons always $argv
     else
         echo "'eza' is not installed on path. Falling back to 'ls'"
         command ls -la $argv
