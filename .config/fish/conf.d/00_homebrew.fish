c2e7feefb52b62043478d9d6a2a2e555bb27de1f
Tim May <98321934+qmg-tmay@users.noreply.github.com>
2026-09-09 09:24:08 +0100

    Task: remove deprecated brew flag (#87)
    * Update: remove deprecated brew flag
    
    * Update: trust third party taps and formulae
    
    * Add: trust to third party taps and formulae

--- .config/fish/conf.d/00_homebrew.fish
+++ .config/fish/conf.d/00_homebrew.fish
@@ -5,7 +5,6 @@ end
 if command --query brew
     set -gx HOMEBREW_BUNDLE_FILE "$HOME/.config/brew/main/Brewfile"
     set -gx HOMEBREW_BUNDLE_NO_LOCK 1
-    set -gx HOMEBREW_NO_REQUIRE_TAP_TRUST 1
 
     abbr brewup "brew update && brew bundle install --upgrade --force-cleanup && brew upgrade"
 end
