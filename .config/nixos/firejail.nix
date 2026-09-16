6a907f22b85ee7d40ceda1b8730da29cc85099f1
Tim <timothy_may@hotmail.co.uk>
2026-09-16 20:08:34 +0100

    Update: configure google-chrome firejail profile

--- .config/nixos/firejail.nix
+++ .config/nixos/firejail.nix
@@ -5,6 +5,7 @@
     wrappedBinaries = {
       google-chrome = {
         executable = "${pkgs.google-chrome}/bin/google-chrome";
+        profile = "${pkgs.firejail}/etc/firejail/google-chrome.profile";
         desktop = "${pkgs.google-chrome}/share/applications/google-chrome.desktop";
       };
     };
