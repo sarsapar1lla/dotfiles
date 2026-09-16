d24531b3ede5b8006b762c3c151b7c45402a209b
Tim <timothy_may@hotmail.co.uk>
2026-09-16 22:14:56 +0100

    Update: rename wrappedBinaries entry

--- .config/nixos/firejail.nix
+++ .config/nixos/firejail.nix
@@ -3,7 +3,7 @@
   programs.firejail = {
     enable = true;
     wrappedBinaries = {
-      google-chrome = {
+      google-chrome-stable = {
         executable = "${pkgs.google-chrome}/bin/google-chrome";
         profile = "${pkgs.firejail}/etc/firejail/google-chrome.profile";
         desktop = "${pkgs.google-chrome}/share/applications/google-chrome.desktop";
